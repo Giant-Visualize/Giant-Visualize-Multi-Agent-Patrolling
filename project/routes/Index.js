a2747136b252c83e3dae14ea5d88a07610807f46
王德正 <Tom@dezhengdeMacBook-Pro.local>
2017-02-13 22:42:21 -0600

    update

--- project/routes/Index.js
+++ project/routes/Index.js
@@ -12,8 +12,8 @@ app.get('/', (req, res) => {
 // });
 
 var server = app.listen(3030, function () {
-   var host = server.address().address
+   var host = '138.49.101.87'
    var port = server.address().port
    
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Giant's app is listening at http://%s:%s", host, port)
 })
