0031ea615cd54c856c87ce55e31bb9d148dcf028
jiangying1019 <jiang.ying@uwlax.edu>
2017-04-02 22:11:21 -0500

    updated algorithm

---
+++ project/update_algorithm/algorithm.js
@@ -0,0 +1,273 @@
+﻿var express = require('express');
+var fs = require('fs');
+var http = require("http");
+var router = express.Router();
+var PF = require('pathfinding');
+
+/*
+ *Don't change the code below 
+ */
+var envMatrix;
+var environment;
+//get the target and get the path to the target
+//set agent pisotion to the target
+//push the result to agentsInfoStore
+//agentsInfoStore: id, region, path
+function chooseTargetsAndGetPaths(agentsInfo, targetList) {
+    var agentsInfoStore = [];
+    var visitedPath = [];
+    var targetListArray = [];
+    for (var i = 0; i < agentsInfo.length; i++) {
+        var target = chooseTarget(agentsInfo[i], targetList);
+        var array = [];
+        visitedPath.push(new visitedPathConstrator(agentsInfo[i].id, agentsInfo[i].region, array));
+        if (target) {
+            agentsInfoStore.push(AgentPathInfo(agentsInfo[i].id, agentsInfo[i].region, shortestPath(agentsInfo[i].position, target)));
+            agentsInfo[i].position = target;
+        }
+    } //end for
+    while (!agentInfoPathIsEmpty(agentsInfoStore)) {
+        targetListArray.push(new copyTargetList(targetList));
+        for (var j = 0; j < agentsInfoStore.length; j++) {
+            if (agentsInfoStore[j].path.length > 0) {
+                var currentNode = agentsInfoStore[j].path.shift();
+                visitedPath[j].path.push(currentNode);
+                targetList = deleteNodeFromTargetList(currentNode, targetList);
+            } else {
+                var tempTarget = chooseTarget(agentsInfo[j], targetList);
+                if (tempTarget) { //find a target
+                    agentsInfoStore[j].path = shortestPath(agentsInfo[j].position, tempTarget);
+                    agentsInfo[j].position = tempTarget;
+                } else { //target is null
+                    agentsInfoStore[j].path = [];
+                }
+            }
+        }
+    }
+    // console.log(JSON.stringify(visitedPath));
+    // console.log((targetListArray));
+    return visitedPath;
+}
+
+
+function AgentPathInfo(id, region, path) {
+    var agent = {};
+    agent.id = id;
+    agent.region = region;
+    agent.path = path;
+    return agent;
+}
+//find a target for an agent.
+//The rule to choose target is that choose the every posotoin in the targetList
+function chooseTarget(agent, targetList) {
+    var target;
+    for (var i = 0; i < targetList.length; i++) {
+        if (targetList[i].regionId === agent.region) {
+            target = targetList[i].position;
+            break;
+        }
+    }
+    return target;
+}
+//find the shortest path between startPosi and endPosi
+function shortestPath(startPosi, endPosi) {
+    var grid = new PF.Grid(envMatrix);
+    var finder = new PF.AStarFinder({
+        allowDiagonal: false
+    });
+    //var path = finder.findPath(1, 2, 4, 2, grid);
+    var path = finder.findPath(startPosi.x - 1, startPosi.y - 1, endPosi.x - 1, endPosi.y - 1, grid);
+    return path;
+}
+
+//get the 0,1 environment matrix for display purpose and for finding the shortest path
+//the code readEnvironment can be reused
+function readEnvironment(environment) {
+    envMatrix = [];
+    var sizeX = environment.size.x;
+    var sizeY = environment.size.y;
+    for (var i = 0; i < sizeY; i++) {
+        envMatrix[i] = [];
+        for (var j = 0; j < sizeX; j++) {
+            envMatrix[i][j] = 1;
+        }
+    }
+    for (var m = 0; m < environment.regions.length; m++) {
+        for (var n = 0; n < environment.regions[m].openSpaces.length; n++) {
+            var openspaceX = environment.regions[m].openSpaces[n].y - 1;
+            var openspaceY = environment.regions[m].openSpaces[n].x - 1;
+            envMatrix[openspaceX][openspaceY] = 0;
+        }
+    }
+}
+//get the agent information
+function getAgentsInfo(environment) {
+    var agentsInfo = [];
+    for (var i = 0; i < environment.agents.length; i++) {
+        agentsInfo.push(environment.agents[i]);
+    }
+    return agentsInfo;
+}
+
+//create a target
+function Target(regionId, position) {
+    var target = {};
+    target.regionId = regionId;
+    target.position = position;
+    return target;
+}
+
+//get target List
+function getTargetList(environment) {
+    var targetList = [];
+    var agentsPosition = [];
+    for (var i = 0; i < environment.agents.length; i++) {
+        var agentPosi = environment.agents[i].position;
+        var str = '[' + agentPosi.x + ',' + agentPosi.y + ']';
+        agentsPosition.push(str);
+    }
+    for (var m = 0; m < environment.regions.length; m++) {
+        for (var n = 0; n < environment.regions[m].openSpaces.length; n++) {
+            var posi = environment.regions[m].openSpaces[n];
+            var posiStr = '[' + posi.x + ',' + posi.y + ']';
+            if (!agentsPosition.includes(posiStr)) { //!agentsPosition.includes(posi)
+                targetList.push(Target(environment.regions[m].id, posi));
+            }
+        }
+    }
+    return targetList;
+}
+
+function Node(x, y) {
+    var node = {};
+    node.x = x;
+    node.y = y;
+    return node;
+}
+
+function AgentInfo(id, position, region) {
+    this.id = id;
+    this.position = position;
+    this.region = region;
+}
+
+function copyAgentsInfo(agentsInfo) {
+    var copyAgentsInfoArray = [];
+    for (var i = 0; i < agentsInfo.length; i++) {
+        copyAgentsInfoArray.push(new AgentInfo(agentsInfo[i].id, agentsInfo[i].position, agentsInfo[i].region));
+    }
+    return copyAgentsInfoArray;
+}
+
+/**
+ **********************Add for changed environment*******************
+ */
+
+//algorithm constrain-3
+function visitedPathConstrator(agentId, agentRegion, path) {
+    this.agentId = agentId;
+    this.agentRegion = agentRegion;
+    this.path = path;
+}
+
+function copyTargetList(targetList) {
+    var copy = [];
+    for (var i = 0; i < targetList.length; i++) {
+        copy.push(Target(targetList[i].regionId, targetList[i].position));
+    }
+    return copy;
+}
+
+function chooseTargetConstrain3(agent, targetList) {
+    var target = null,
+        pathLength = 0;
+    //find the farest targrt
+    for (var j = 0; j < targetList.length; j++) {
+        if (targetList[j].regionId === agent.region) {
+            var distance = shortestPath(agent.position, targetList[j].position).length;
+            if (pathLength < distance) {
+                target = targetList[j].position;
+                pathLength = distance;
+            }
+        }
+    }
+    return target;
+}
+
+function constrain3GetPath(agentsInfo, targetList) {
+    var agentsInfoStore = [];
+    var visitedPath = [];
+    var targetListArray = [];
+    for (var i = 0; i < agentsInfo.length; i++) {
+        var target = chooseTargetConstrain3(agentsInfo[i], targetList);
+        var array = [];
+        visitedPath.push(new visitedPathConstrator(agentsInfo[i].id, agentsInfo[i].region, array));
+        if (target) {
+            agentsInfoStore.push(AgentPathInfo(agentsInfo[i].id, agentsInfo[i].region, shortestPath(agentsInfo[i].position, target)));
+            agentsInfo[i].position = target;
+        }
+    } //end for
+    while (!agentInfoPathIsEmpty(agentsInfoStore)) {
+        targetListArray.push(new copyTargetList(targetList));
+        for (var j = 0; j < agentsInfoStore.length; j++) {
+            if (agentsInfoStore[j].path.length > 0) {
+                var currentNode = agentsInfoStore[j].path.shift();
+                visitedPath[j].path.push(currentNode);
+                targetList = deleteNodeFromTargetList(currentNode, targetList);
+            } else {
+                var tempTarget = chooseTargetConstrain3(agentsInfo[j], targetList);
+                if (tempTarget) { //find a target
+                    agentsInfoStore[j].path = shortestPath(agentsInfo[j].position, tempTarget);
+                    agentsInfo[j].position = tempTarget;
+                } else { //target is null
+                    agentsInfoStore[j].path = [];
+                }
+            }
+        }
+    }
+    //console.log(JSON.stringify(visitedPath));
+    //console.log((targetListArray));
+    return visitedPath;
+}
+
+function deleteNodeFromTargetList(node, targetList) {
+    for (var i = 0; i < targetList.length; i++) {
+        if (targetList[i].position.x - 0 === node[0] + 1 && targetList[i].position.y - 0 === node[1] + 1) {
+            targetList.splice(i, 1);
+        }
+    }
+    return targetList;
+}
+
+function agentInfoPathIsEmpty(agentsInfoStore) {
+    for (var i = 0; i < agentsInfoStore.length; i++) {
+        if (agentsInfoStore[i].path.length !== 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function getAgentPath(env) {
+    environment = env;
+    readEnvironment(env);
+    var targetList = getTargetList(env);
+    var agentsInfo = getAgentsInfo(env);
+    var copyAgentInfo = copyAgentsInfo(agentsInfo);
+    var agentsInfoStore = chooseTargetsAndGetPaths(copyAgentInfo, targetList); //return to client
+
+    return agentsInfoStore;
+}
+
+module.exports.getAgentPath = getAgentPath;
+
+function constrain3(env) {
+    var agentsInfo1 = getAgentsInfo(env);
+    var targetList1 = getTargetList(env);
+    var agentsInfoConstrain3 = constrain3GetPath(agentsInfo1, targetList1);
+
+}
+module.exports.constrain3 = constrain3;
+
+//Don't change the code above
---
+++ project/update_algorithm/readFile.js
@@ -0,0 +1,182 @@
+﻿/**
+ * Don't change the code below
+ */
+var environment;
+var agentPath;
+var s = [];
+
+function readFile() {
+    var string;
+    // var file = document.getElementById("fileUpload").files[0];
+    var file = $('#fileUpload').get(0).files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = function(evt) {
+            string = evt.target.result;
+            //alert(string);
+            environment = processFile(string);
+            //read environment change it to matrix
+            // readEnvironment(environment);  // works fine
+        }
+        reader.onerror = function(evt) {
+            alert("error reading file");
+        }
+    } else return;
+}
+
+
+//works fine
+//get the information of environment
+function processFile(string) {
+    var lines = string.split('\n');
+    var size = Size(lines[0].match(/\d+/g, '')[0], lines[0].match(/\d+/g, '')[1]);
+    //environment size is correct
+    var agents = [];
+    var regions = [];
+    var environment = {};
+    var currentRegionID;
+
+    var i = 1;
+    while (i < lines.length) {
+        var openSpacesOfOneRegion = [];
+        if (lines[i].includes('Region')) {
+            currentRegionID = lines[i].match(/\d+/g, '')[0];
+            i++;
+        }
+        if (lines[i].includes('Coordinates:')) {
+            var coordinates = lines[i].match(/\d+/g, '');
+            for (var j = 0; j < coordinates.length; j = j + 2) {
+                var posi = Position(coordinates[j], coordinates[j + 1]);
+                openSpacesOfOneRegion.push(posi);
+            }
+            var region = Region(currentRegionID, openSpacesOfOneRegion);
+            regions.push(region);
+            i++;
+        }
+        if (lines[i].includes('Agents')) {
+            i++;
+        }
+        if (lines[i].includes('Agent')) {
+            var agentId = lines[i].match(/\d+/g, '')[0];
+            var agentPosition = Position(lines[i].match(/\d+/g, '')[1], lines[i].match(/\d+/g, '')[2]);
+            agents.push(Agent(agentId, agentPosition, currentRegionID));
+            i++;
+        } else {
+            i++;
+        }
+    }
+    environment.size = size;
+    environment.regions = regions;
+    environment.agents = agents;
+    if (!verifyFile(environment)) {
+        return null;
+    }
+    return environment;
+}
+
+function verifyFile(env) {
+    var agents = env.agents;
+    var regions = env.regions;
+    var size = env.size;
+    //region position is invalid, like the index is less or more than size
+    for (var i = 0; i < regions.length; i++) {
+        var currentRegion = regions[i];
+        for (var m = 0; m < currentRegion.openSpaces.length; m++) {
+            if (currentRegion.openSpaces[m].x - 0 < 1 || currentRegion.openSpaces[m].x - 0 > size.x - 0 || currentRegion.openSpaces[m].y - 0 < 1 || currentRegion.openSpaces[m].y - 0 > size.y - 0) {
+                alert('Invalid region');
+                return false;
+            }
+        }
+    }
+    //agents information is correct or not
+    for (var j = 0; j < agents.length; j++) {
+        var agentRegion = agents[j].region;
+        //agent start position is invalid, like the index is less or more than size
+        if (agents[j].position.x - 0 < 1 || agents[j].position.x - 0 > size.x - 0 || agents[j].position.y - 0 < 1 || agents[j].position.y - 0 > size.y - 0) {
+            alert('Invalid agent position');
+            return false;
+        }
+
+        //agent start position is not in the region
+        for (var k = 0; k < regions.length; k++) { //find the region that the agent belongs to
+            if (regions[k].id === agentRegion) {
+                var n = 0;
+                for (n = 0; n < regions[k].openSpaces.length; n++) { //check whether the agent is in that region
+                    if ((regions[k].openSpaces[n].x - 0 === agents[j].position.x - 0) && regions[k].openSpaces[n].y - 0 === agents[j].position.y - 0) {
+                        break;
+                    }
+                }
+                if (n === regions[k].openSpaces.length) {
+                    alert("Agent position is not in the region");
+                    return false;
+                }
+            }
+        }
+    }
+    //one region has two same position
+    return true;
+}
+
+function Position(x, y) {
+    var position = {};
+    position.x = x;
+    position.y = y;
+    return position;
+}
+
+function Size(x, y) {
+    var size = {};
+    size.x = x;
+    size.y = y;
+    return size;
+}
+
+function Agent(id, position, region) {
+    var agent = {};
+    agent.id = id;
+    agent.position = position;
+    agent.region = region;
+    return agent;
+}
+
+function Region(id, openSpaces) {
+    var region = {};
+    region.id = id;
+    region.openSpaces = openSpaces;
+    return region;
+}
+/**
+ * read environment get the environment 0,1 matrix
+ */
+
+function getSettings() {
+    if (environment === null)
+        return;
+    $.ajax({
+        url: "/file",
+        method: "GET",
+        data: { environment: environment },
+        success: showAgentsPath,
+        error: function(data) {
+            alert("error");
+        }
+    });
+}
+
+//Don't change the code above
+
+function getEnvironment() {
+    return environment;
+}
+
+function getAgentPath() {
+    return agentPath;
+}
+//allAgentsPaths below is the data you need to display
+function showAgentsPath(allAgentsPaths) {
+    paper = Raphael("holderOfBlock", 1280, 680);
+    drawEnvironment(getEnvironment());
+    showGuidelines(getEnvironment());
+    agentPath = allAgentsPaths;
+}
